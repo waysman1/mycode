46ffad750e66eeccb04a181d0adfc4c213a7ce7f
Mike Ways <Lovern.M.Ways@verizon.com>
2019-07-23 15:23:58 -0400

    your commit message

---
+++ if-test/condition03.py
@@ -0,0 +1,14 @@
+#1/usr/bin/env python3
+
+## Collect input from the user
+hostname = input("What value should we set for hostname?")
+
+## here we use the str.lower() method to return a lowercase string
+
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    print("hostname was found to be mtg")
+
+##AA Always print out to the user
+print("Exiting the script")
+
